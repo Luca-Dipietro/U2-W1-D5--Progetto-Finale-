796d08f2da71a082a5566973d4fcff39be455cde
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-03-29 22:50:15 +0100

    adds scrolling events e position sticky for nav and aside

--- Clone Medium Page/scritp.js
+++ Clone Medium Page/scritp.js
@@ -1,11 +1,14 @@
 // Evento scroll
 
 const navBar = document.querySelector(".nav");
+const btnNav = document.querySelector(".navB");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 425) {
-    navBar.classList.add("scrolled");
+  if (window.scrollY > 380) {
+    navBar.classList.add("scrolled1");
+    btnNav.classList.add("scrolled2");
   } else {
-    navBar.classList.remove("scrolled");
+    navBar.classList.remove("scrolled1");
+    btnNav.classList.remove("scrolled2");
   }
 });
