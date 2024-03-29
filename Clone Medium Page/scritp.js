613f851d959b92df027402368e3d617f2b6a614a
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-03-29 23:20:07 +0100

    Try second extra

--- Clone Medium Page/scritp.js
+++ Clone Medium Page/scritp.js
@@ -1,4 +1,4 @@
-// Evento scroll
+// Event scroll
 
 const navBar = document.querySelector(".nav");
 const btnNav = document.querySelector(".navB");
@@ -12,3 +12,18 @@ window.addEventListener("scroll", function () {
     btnNav.classList.remove("scrolled2");
   }
 });
+
+// Event letters
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomOpacity() {
+  const letters = document.querySelectorAll("#random-letters");
+  letters.forEach((letter) => {
+    letter.setAttribute("opacity", getRandom(0, 1));
+  });
+}
+
+setInterval(randomOpacity, 2000);
